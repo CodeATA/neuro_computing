e13f93d964dc0834b11633d5867c38d9b8d2c783
CodeATA <cdwdhr@gmail.com>
2017-12-27 20:39:22 +0800

    初步的模拟器
    可以运行，存在一个warning（关于更新权重时使用的迭代变量i）
    没有写数据记录和做图

--- code/brian2/sim.py
+++ code/brian2/sim.py
@@ -1,40 +1,48 @@
-import brian2
+from brian2 import *
 import matplotlib
 
 # one type of synapse has only one delay
 
-synapse_num = int(input("input synapse number: "))
-neuron_num = int(input("input neuron number: "))
+synapse_num = 10
+neuron_num = 5
+
+tau = 10*ms
 
 eqs = '''
-
+dv/dt = (v0 - v) / tau : volt (unless refractory)
+v0 : volt
 '''
-threshold = ''' '''
-reset = ''' '''
+threshold = '''v > 10*mV'''
+reset = '''v = 0*mV'''
 refractory = 5*ms
 
-G = NeuronGroup(neuron_num, eqs, threshold=threshold, reset=reset, metho='exact')
+G = NeuronGroup(neuron_num, eqs, threshold=threshold, reset=reset, method='exact')
 
-s_model = '''w : 1'''
-on_pre = ''' '''
+s_model = '''w : volt'''
+on_pre = '''v_post += w'''
 on_post = ''' '''
 
-S = Synapses(G, G, model=s_model, on_pre=on_pre)
+S = Synapses(G, G, model=s_model, on_pre=on_pre, on_post=on_post)
 
 pre = []
 post = []
 weight = []
 # unfinished
-for i in range(0, synapse_num):
-  pre.append()
-  post.append()
-  weight.append()
-S.connect(i=pre, j=post)
-for i in range(0, synapse_num):
-  S.w[pre[i], post[i]] = weight[i]
+with open("synapases", "r") as src:
+  for line in src:
+    split_line = line.split(',')
+    pre.append(int(split_line[0]))
+    post.append(int(split_line[1]))
+    weight.append(float(split_line[2]))
+  print(pre)
+  print(post)
+  print(weight)
+  S.connect(i=pre, j=post)
+  for i in range(0, synapse_num):
+    S.w[pre[i], post[i]] = weight[i]*mV
 
-stimuli = PoissonGroup(neuron_num, np.arange(100)*Hz + 10*Hz)
-sti_input = Synapses(stimuli, G, on_pre='v_post+=0.1')
+stimuli = PoissonGroup(neuron_num, np.arange(neuron_num)*Hz + 10*Hz)
+sti_input = Synapses(stimuli, G, on_pre='v_post+=0.1*mV')
 sti_input.connect(j='i')
 
 # for recoding
